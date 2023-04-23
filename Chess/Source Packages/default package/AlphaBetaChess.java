072c07d7b18c12fc578a1d991f9dc0faac6b53c8
Aman <Palaman2626@gmail.com>
2023-04-23 10:34:09 +0530

    Fourth Commit

--- Chess/Source Packages/default package/AlphaBetaChess.java
+++ Chess/Source Packages/default package/AlphaBetaChess.java
@@ -1,3 +1,4 @@
+import java.util.*;
 //import javax.swing.*;
 public class AlphaBetaChess {
     static String chessBoard[][]={
@@ -35,6 +36,31 @@ public class AlphaBetaChess {
         f.setSize(500, 500);
         f.setVisible(true);*/
         System.out.println(posibleMoves());
+        makeMove("7655 ");
+        undoMove("7655 ");
+        for (int i=0;i<8;i++) {
+            System.out.println(Arrays.toString(chessBoard[i]));
+        }
+    }
+    public static void makeMove(String move) {
+        if (move.charAt(4)!='P') {
+            chessBoard[Character.getNumericValue(move.charAt(2))][Character.getNumericValue(move.charAt(3))]=chessBoard[Character.getNumericValue(move.charAt(0))][Character.getNumericValue(move.charAt(1))];
+            chessBoard[Character.getNumericValue(move.charAt(0))][Character.getNumericValue(move.charAt(1))]=" ";
+        } else {
+            //if pawn promotion
+            chessBoard[1][Character.getNumericValue(move.charAt(0))]=" ";
+            chessBoard[0][Character.getNumericValue(move.charAt(1))]=String.valueOf(move.charAt(3));
+        }
+    }
+    public static void undoMove(String move) {
+        if (move.charAt(4)!='P') {
+            chessBoard[Character.getNumericValue(move.charAt(0))][Character.getNumericValue(move.charAt(1))]=chessBoard[Character.getNumericValue(move.charAt(2))][Character.getNumericValue(move.charAt(3))];
+            chessBoard[Character.getNumericValue(move.charAt(2))][Character.getNumericValue(move.charAt(3))]=String.valueOf(move.charAt(4));
+        } else {
+            //if pawn promotion
+            chessBoard[1][Character.getNumericValue(move.charAt(0))]="P";
+            chessBoard[0][Character.getNumericValue(move.charAt(1))]=String.valueOf(move.charAt(2));
+        }
     }
     public static String posibleMoves() {
         String list="";
